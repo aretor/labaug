100fc9548cc01fced3df0206f2f5cdbe4e7942db
aretor <ale.torcinovich@unive.it>
2018-10-17 10:06:40 +0200

    add cycles through exps, percs and models

--- CNN.py
+++ CNN.py
@@ -8,39 +8,45 @@ import config as cfg
 from extract_tools import prepare_loader, get_finetune_model
 import net
 
-
 dset_name = 'caltech'
 dataset = cfg.DSETS[dset_name]
-model_name = 'resnet18'
-# model_name = 'densenet121'
+model_names = ['resnet18', 'densenet121']
 data_dir = osp.join(cfg.DATA_DIR, dset_name)
 feature_dir = osp.join(data_dir, cfg.FEATURE_DIR)
 label_dir = osp.join(data_dir, cfg.LABEL_DIR)
 net_dir = osp.join(data_dir, cfg.NET_DIR)
 epochs = 10
 batch_size = 8
-ind = 0
-tr_perc = 0.02
+tr_percs = [0.02, 0.05, 0.1]
 
 if not osp.exists(net_dir):
     os.makedirs(net_dir)
 
-for alg in ['gtg', 'svm', 'labeled_only']:
-    train_loader = prepare_loader(osp.join(label_dir, alg, model_name, 'labels_{}_{}.txt'.format(tr_perc, ind)),
-                                  '', dataset['stats'], batch_size, shuffle=True, sep=',')
+for exp in range(cfg.NUM_EXPS):
+    ts_lab_path = osp.join(label_dir, 'test_labels_{}.txt'.format(exp))
+    for model_name in model_names:
+        for tr_perc in tr_percs:
+            for alg in ['gtg', 'svm', 'labeled_only']:
+                lab_path = osp.join(label_dir, alg, model_name,
+                                    'labels_{}_{}.txt'.format(tr_perc, exp))
 
-    test_loader = prepare_loader(osp.join(label_dir, 'test_labels_{}.txt'.format(ind)), '', dataset['stats'], batch_size,
-                                 shuffle=False, sep=',')
+                train_loader = prepare_loader(lab_path, '', dataset['stats'],
+                                              batch_size, shuffle=True, sep=',')
 
-    model = get_finetune_model(model_name, dataset['nr_classes'])
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+                test_loader = prepare_loader(ts_lab_path, '', dataset['stats'],
+                                             batch_size, shuffle=False, sep=',')
 
-    trained_net = net.train(model, model_name, train_loader, test_loader, optimizer, criterion, epochs, net_dir, ind)
+                model = get_finetune_model(model_name, dataset['nr_classes'])
+                criterion = nn.CrossEntropyLoss()
+                optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
-    model.load_state_dict(torch.load(trained_net))
-    net_accuracy = net.evaluate(model, test_loader)
-    print('Final accuracy: {}'.format(net_accuracy))
+                trained_net = net.train(model, model_name, train_loader,
+                                        test_loader, optimizer, criterion,
+                                        epochs, net_dir, exp)
+
+                model.load_state_dict(torch.load(trained_net))
+                net_accuracy = net.evaluate(model, test_loader)
+                print('Final accuracy: {}'.format(net_accuracy))
 
 # # do the same thing but with a linear SVM
 # svm_linear_classifier = svm.LinearSVC()

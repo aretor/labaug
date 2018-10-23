5314820f6d5b47295d26bc30a7656452a927919c
aretor <sebastiano.vascon@unive.it>
2018-10-23 11:29:42 +0200

    move parameter hard_labels

--- trainer.py
+++ trainer.py
@@ -1,21 +1,100 @@
+import sys
 import os
 import os.path as osp
+from tqdm import tqdm
 import numpy as np
+from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.autograd import Variable
 
 from extract_tools import get_finetune_model, prepare_loader
-import net
 
 
 class Trainer(object):
-    def __init__(self, dset, label_dir, net_dir, res_dir, net_names):
+    def __init__(self, dset, label_dir, net_dir, res_dir, net_names, hard_labels):
         self.dset = dset
         self.net_names = net_names
         self.label_dir = label_dir
         self.net_dir = net_dir
         self.res_dir = res_dir
+        self.hard_labels = hard_labels
+
+        self.train_loader = None
+        self.test_loader = None
+
+    def train(self, net, net_type, lab_path, epochs, lr, batch_size):
+        net.train()
+        net_name = osp.join(self.net_dir, net_type + '.pth')
+        ts_lab_path = osp.join(self.label_dir, 'test_labels.txt')
+
+        criterion = nn.CrossEntropyLoss() if self.hard_labels else nn.KLDivLoss()
+        optimizer = optim.Adam(net.parameters(), lr=lr)
+
+        if not self.hard_labels:
+            logsoftmax = torch.nn.LogSoftmax(dim=1)
+
+        self.train_loader = prepare_loader(lab_path, '', self.dset['stats'], batch_size, shuffle=True,
+                                           sep=',', hard_labels=self.hard_labels)
+
+        self.test_loader = prepare_loader(ts_lab_path, '', self.dset['stats'], batch_size, shuffle=False,
+                                          sep=',', hard_labels=self.hard_labels)
+
+        print("Validating results in: start")
+        sys.stdout.flush()
+        accuracy = self.evaluate(net)[0]
+        print(accuracy)
+
+        for epoch in range(epochs):
+            net.train()
+            print('{} --------- Epoch: {}'.format(net_type, epoch))
+            running_loss = 0.0
+            for i, data in tqdm(enumerate(self.train_loader), total=len(self.train_loader)):
+                inputs, labels, _ = data
+                inputs, labels = Variable(inputs).cuda(), Variable(labels).cuda()
+                optimizer.zero_grad()
+                outputs = net(inputs)
+                if net_type == 'inception':
+                    outputs = outputs[0] + outputs[1]
+
+                if not self.hard_labels:
+                    outputs = logsoftmax(outputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss += loss.data[0]
+            sys.stderr.flush()
+            print('[%d] loss: %.16f' % (epoch + 1, running_loss / len(self.train_loader)))
+            print("Validating results in: %d-th epoch" % epoch)
+            sys.stdout.flush()
+            accuracy = self.evaluate(net)[0]
+            print("Accuracy: %f" % accuracy)
+
+            torch.save(net.state_dict(), net_name)
+
+        return net_name
+
+    def evaluate(self, net):
+        net.eval()
+        predictions, gts = [], []
+        for data in tqdm(self.test_loader, total=len(self.test_loader)):
+            inputs, labels, _ = data
+            inputs = Variable(inputs).cuda()
+            outputs = net(inputs)
+            _, predicted = torch.max(outputs.data, dim=1)
+            predictions.append(predicted)
+            gts.append(labels)
+        predictions = torch.cat(predictions, dim=0).cpu().numpy()
+        gts = torch.cat(gts, dim=0).numpy()
+
+        accuracy = (predictions == gts).sum() / float(len(gts))
+        P, R, F1, _ = precision_recall_fscore_support(gts, predictions)
+        conf_mat = confusion_matrix(gts, predictions)
+
+        return accuracy, P, R, F1, conf_mat
 
     def __call__(self, *args, **kwargs):
         tr_percs = kwargs.pop('tr_percs', [0.02, 0.05, 0.1])
@@ -23,12 +102,10 @@ class Trainer(object):
         batch_size = kwargs.pop('batch_size_tr', 8)
         epochs = kwargs.pop('epochs', 10)
         lr = kwargs.pop('lr', 1e-4)
-        hard_labels = kwargs.pop('hard_labels', True)
 
         if not osp.exists(self.net_dir):
             os.makedirs(self.net_dir)
 
-        ts_lab_path = osp.join(self.label_dir, 'test_labels.txt')
         for net_name in self.net_names:
             res_model_dir = osp.join(self.res_dir, net_name)
             if not osp.exists(res_model_dir):
@@ -36,38 +113,16 @@ class Trainer(object):
 
             for tr_perc in tr_percs:
                 for alg in algs:
-                    lab_path = osp.join(self.label_dir, alg, net_name,
-                                        'labels_{}.txt'.format(tr_perc))
-
-                    train_loader = prepare_loader(lab_path, '', self.dset['stats'],
-                                                  batch_size, shuffle=True,
-                                                  sep=',', hard_labels=hard_labels)
-
-                    test_loader = prepare_loader(ts_lab_path, '', self.dset['stats'],
-                                                 batch_size, shuffle=False,
-                                                 sep=',', hard_labels=hard_labels)
+                    lab_path = osp.join(self.label_dir, alg, net_name, 'labels_{}.txt'.format(tr_perc))
 
                     model = get_finetune_model(net_name, self.dset['nr_classes'])
-                    criterion = nn.CrossEntropyLoss() if hard_labels else\
-                        nn.KLDivLoss()
-                    optimizer = optim.Adam(model.parameters(), lr=lr)
-
-                    trained_net = net.train(model, net_name, train_loader,
-                                            test_loader, optimizer, criterion,
-                                            epochs, self.net_dir, hard_labels=hard_labels)
+                    trained_net = self.train(model, net_name, lab_path, epochs, lr=lr, batch_size=batch_size)
 
                     model.load_state_dict(torch.load(trained_net))
-                    accuracy, P, R, F1, conf_mat = net.evaluate(model,
-                                                                test_loader)
+                    accuracy, P, R, F1, conf_mat = self.evaluate(model)
 
-                    with open(osp.join(res_model_dir,
-                                       'res_{}_{}.txt'.format(alg, tr_perc)),
-                              'w') as res:
-                        res.write('\nACC,P,R,F1\n')
-                        res.write(
-                            '{},{},{},{}\n'.format(accuracy, P, R, F1))
+                    with open(osp.join(res_model_dir, 'res_{}_{}.txt'.format(alg, tr_perc)), 'w') as res:
+                        res.write('ACC,P,R,F1\n')
+                        res.write('{},{},{},{}\n'.format(accuracy, P, R, F1))
 
-                    np.savetxt(osp.join(res_model_dir,
-                                        'conf_mat_{}_{}.txt'.format(alg,
-                                                                    tr_perc)),
-                               conf_mat)
+                    np.savetxt(osp.join(res_model_dir, 'conf_mat_{}_{}.txt'.format(alg, tr_perc)), conf_mat)

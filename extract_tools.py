b767aad586c4da69f71b0492745605d416a07844
aretor <ale.torcinovich@unive.it>
2018-10-23 08:24:31 +0200

    delete create_net

--- extract_tools.py
+++ extract_tools.py
@@ -17,16 +17,6 @@ def get_finetune_model(net, nr_classes):
     return model.cuda()
 
 
-def create_net(number_of_classes, nets_and_features, net_type='resnet152'):
-    net, feature_size = nets_and_features[net_type][0](pretrained=True), nets_and_features[net_type][1]
-    if net_type[:3] == 'den':
-        net.classifier = nn.Linear(feature_size, number_of_classes)
-    else:
-        net.fc = nn.Linear(feature_size, number_of_classes)
-    net = net.cuda()
-    return net, feature_size
-
-
 # TODO: change collate_fn to work correctly with FileImageFolder (the path is returned as a list instead of str)
 def prepare_loader(dataset_path, img_root, stats, batch_size, shuffle, sep='',
                    hard_labels=True):
